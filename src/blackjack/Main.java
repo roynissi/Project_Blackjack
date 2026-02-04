6b48d19dbfd7a43c72c2591541615155e3661913
roynissi <nissir@sheridancollege.ca>
2026-02-03 20:39:58 -0500

    Add ASCII title screen with play prompt

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -1,7 +1,26 @@
 package blackjack;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Blackjack!");
+        // Title Screen
+        System.out.println("================================");
+        System.out.println("           BLACKJACK            ");
+        System.out.println("================================");
+        System.out.println();
+        System.out.println("        Press ENTER to Play     ");
+        System.out.println();
+
+        //Game Initialization
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        System.out.println("\n\n\n");
+        System.out.println("        Shuffling Cards....     ");
+
+
+
     }
 }
