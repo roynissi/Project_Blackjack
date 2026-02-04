013ad8d8f19858553ad1baba9b07f52fb858b3c7
roynissi <nissir@sheridancollege.ca>
2026-02-03 20:48:54 -0500

    Add ASCII game screen layout

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -20,6 +20,23 @@ public class Main {
         System.out.println("\n\n\n");
         System.out.println("        Shuffling Cards....     ");
 
+        // Game Screen Layout to be replaced with logic
+        System.out.println("\n");
+        System.out.println("Dealer's Hand:");
+        System.out.println("[??] [7♠]");
+        System.out.println();
+
+        System.out.println("-------------------------------");
+        System.out.println();
+
+        System.out.println("Your Hand:");
+        System.out.println("[10♦] [6♥]");
+        System.out.println("Total: 16");
+        System.out.println();
+
+        System.out.println("(H) Hit    (S) Stand");
+
+
 
 
     }
