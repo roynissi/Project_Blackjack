12773a04db2cc2f6866eb19a7e084d48b5bac5a5
kellyennisjr-lang <kellyennisjr@gmail.com>
2026-04-07 15:49:00 -0400

    Add files via upload

---
+++ tests/blackjack/GameController/DealerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package blackjack.GameController;
+
+import java.util.Scanner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kelly
+ */
+public class DealerTest {
+    
+    public DealerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of wantsToHit method, of class Dealer.
+     */
+    @Test
+    public void testWantsToHit() {
+        System.out.println("wantsToHit");
+        Scanner scanner = null;
+        Dealer instance = new Dealer();
+        boolean expResult = false;
+        boolean result = instance.wantsToHit(scanner);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
